82f8ab9abdc87aeb41af2de5e3744545fd318ae3
Hassane Bendaoud <40140987+realHassaneBen@users.noreply.github.com>
2022-06-10 12:51:32 +0100

    host & schema

--- sanity/schemas/host.js
+++ sanity/schemas/host.js
@@ -1,6 +1,6 @@
 export default {
-  name: 'host',
-  title: 'Host',
-  type: 'reference',
-  to: [{ type: 'person' }],
-}
+    name: 'host',
+    title: 'Host',
+    type: 'reference',
+    to: [{ type: 'person' }],
+};
